0f803b0efe3787bdb524de85e224aa943d4a66de
M00nNight <khanzamil267@gmail.com>
2024-03-04 17:06:26 +0530

    Added:File upload form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,48 @@ import { SignOutButton, SignedIn, SignedOut } from "@clerk/clerk-react";
 import { SignInButton, useOrganization, useUser } from "@clerk/nextjs";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  title: z.string().min(2).max(50),
+  file: z.instanceof(FileList),
+});
 
 export default function Home() {
   const organization = useOrganization();
   const user = useUser();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+    },
+  });
+  const fileRef = form.register("file");
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
     orgId = organization.organization?.id ?? user.user?.id;
@@ -15,8 +53,73 @@ export default function Home() {
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile);
   return (
-    <main className=" flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className=" text-4xl font-bold "> Your Files</h1>
+    <main className=" container mx-auto pt-12">
+      <div className=" flex justify-between items-center">
+        <h1 className=" text-4xl font-bold "> Your Files</h1>
+
+        <Dialog>
+          <DialogTrigger>
+            <Button
+              onClick={() => {
+                if (!orgId) {
+                  return;
+                }
+                createFile({
+                  name: "Hello World!",
+                  orgId,
+                });
+              }}
+            >
+              Upload File
+            </Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Upload your file here</DialogTitle>
+              <DialogDescription>
+                <Form {...form}>
+                  <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8"
+                  >
+                    <FormField
+                      control={form.control}
+                      name="title"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <Input placeholder="Testing" {...field} />
+                          </FormControl>
+
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="file"
+                      render={({ field: { onChange }, ...field }) => (
+                        <FormItem>
+                          <FormLabel>File</FormLabel>
+                          <FormControl>
+                            <Input type="file" {...fileRef} />
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+
+                    <Button type="submit">Submit</Button>
+                  </form>
+                </Form>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+
+        {/*
+         */}
+      </div>
       <SignedIn>
         <SignOutButton>
           <Button>Sign Out</Button>
@@ -30,19 +133,6 @@ export default function Home() {
       {files?.map((file) => {
         return <div key={file._id}>{file.name}</div>;
       })}
-      <Button
-        onClick={() => {
-          if (!orgId) {
-            return;
-          }
-          createFile({
-            name: "Hello World!",
-            orgId,
-          });
-        }}
-      >
-        Create File
-      </Button>
     </main>
   );
 }
